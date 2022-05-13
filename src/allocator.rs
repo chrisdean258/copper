f4ed6a633e5704bfa00c19be8050e9e2e4ae2226
Chris Dean <chrisdean258@gmail.com>
2022-05-13 16:38:12 -0400

    stub out alloctor

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,14 +1,17 @@
 #![allow(dead_code)]
 use std::collections::HashSet;
+use std::ops::{Index, IndexMut};
 
 pub struct Allocator<T> {
+    base: usize,
     memory: Vec<T>,
     free: HashSet<usize>,
 }
 
 impl<T> Allocator<T> {
-    pub fn new() -> Self {
+    pub fn new(base: usize) -> Self {
         Self {
+            base,
             memory: Vec::new(),
             free: HashSet::new(),
         }
@@ -37,5 +40,32 @@ impl<T> Allocator<T> {
 
     pub fn free(&mut self, addr: usize) {
         self.free.insert(addr);
+    }
+}
+
+impl<T> Index<usize> for Allocator<T> {
+    type Output = T;
+
+    fn index(&self, addr: usize) -> &Self::Output {
+        &self.memory[addr - self.base]
+    }
+}
+
+impl<T> IndexMut<usize> for Allocator<T> {
+    fn index_mut(&mut self, addr: usize) -> &mut Self::Output {
+        &mut self.memory[addr - self.base]
+    }
+}
+
+impl<T> IntoIterator for Allocator<T> {
+    type Item = T;
+
+    fn iter(&mut self) -> Vec<T>::IntoIter {
+        self.memory
+            .iter()
+            .enumerate()
+            .filter(|(i, v)| !self.free.contains(&i))
+            .collect()
+            .into_iter()
     }
 }
