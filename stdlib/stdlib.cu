9d7c0bde5a57cfdfba373cab4b8caafef874307b
Chris Dean <chrisdean258@gmail.com>
2022-06-06 19:29:50 -0400

    now using variable argument print/prints in stdlib

--- stdlib/stdlib.cu
+++ stdlib/stdlib.cu
@@ -2,10 +2,13 @@
 	# write(a1, a2, a3, a4, a5, a6, a7, a8, '\n')
 # }
 
-fn print(a1="", a2="", a3="", a4="", a5="") {
-	write(1, a1, a2, a3, a4, a5, '\n')
-}
+fn print(*args) write(1, *args, '\n')
+fn prints(*args) write(1, *args)
 
-fn prints(a1="", a2="", a3="", a4="") {
-	write(1, a1, a2, a3, a4)
-}
+# fn print(a1="", a2="", a3="", a4="", a5="") {
+	# write(1, a1, a2, a3, a4, a5, '\n')
+# }
+
+# fn prints(a1="", a2="", a3="", a4="") {
+	# write(1, a1, a2, a3, a4)
+# }
