bcebc47eaaa92c5bea3622eabbc4bb796e153322
Chris Dean <chrisdean258@gmail.com>
2022-06-21 16:57:09 -0400

    more colorful output for indicating how a test failed

--- run_tests.sh
+++ run_tests.sh
@@ -3,9 +3,25 @@
 
 cargo build || exit 1
 
+# red
+# green
+# yellow
+# blue
+# magenta
+# cyan
+# white
+
+
 run-test()
 {
-	diff -y <(./target/debug/copper "$1" 2>&1) "$1.out"
+	rv="0"
+	if ! diff -y <(./target/debug/copper "$1" 2> stderr.txt) "$1.out"; then
+		rv=$((rv+1))
+	fi
+	if [ -n "$(cat "stderr.txt")" ]; then 
+		rv=$((rv+2))
+	fi
+	return $rv
 }
 
 color() {
@@ -21,9 +37,15 @@ if [ $# -eq 0 ]; then
 
 	for file in ./test/*.cu; do
 		num_tests=$(($num_tests + 1))
-		if run-test "$file" &>/dev/null; then
+		run-test "$file" &>/dev/null
+		rv=$?
+		if [ "$rv" -eq 0 ]; then
 			num_passed="$((num_passed + 1))"
 			color 2 "Passed $file"
+		elif [ "$rv" -eq 1 ]; then
+			color 5 "Failed $file"
+		elif [ "$rv" -eq 2 ]; then
+			color 3 "Failed $file"
 		else
 			color 1 "Failed $file"
 		fi
