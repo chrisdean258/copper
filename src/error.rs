b065935bd35d1de1d52a614720503ab02af96e56
Chris Dean <chrisdean258@gmail.com>
2022-11-08 22:44:37 -0500

    error.rs

---
+++ src/error.rs
@@ -0,0 +1,37 @@
+use std::{
+    default::Default,
+    error::Error,
+    fmt::{Display, Formatter, Result},
+};
+
+#[derive(Clone, Debug)]
+pub struct ErrorCollection<T: Error> {
+    errs: Vec<T>,
+}
+
+impl<T: Error> Error for ErrorCollection<T> {}
+
+impl<T: Error> Display for ErrorCollection<T> {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        for error in self.errs.iter() {
+            f.write_fmt(format_args!("{error}\n"))?;
+        }
+        Ok(())
+    }
+}
+
+impl<T: Error> Default for ErrorCollection<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: Error> ErrorCollection<T> {
+    pub fn new() -> Self {
+        Self { errs: Vec::new() }
+    }
+
+    pub fn push(&mut self, err: T) {
+        self.errs.push(err);
+    }
+}
